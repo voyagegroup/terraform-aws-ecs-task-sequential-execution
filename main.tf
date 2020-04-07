b061e62a1dcce3ce6d2fe6e3c1acea6088f15dd4
pei0804 <peeeei0804@gmail.com>
2020-04-07 19:48:12 +0900

    fix null resouce

--- main.tf
+++ main.tf
@@ -14,4 +14,8 @@ resource "aws_ecs_service" "this" {
     subnets          = var.subnets
     assign_public_ip = var.assign_public_ip
   }
+
+  provisioner "local-exec" {
+    command = "aws ecs update-service --cluster ${aws_ecs_service.this.cluster} --service ${aws_ecs_service.this.name} --task-definition ${data.aws_ecs_task_definition.this.id} --force-new-deployment"
+  }
 }
